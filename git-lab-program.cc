3a65b4e3f65720ac8e14be292223115a3647d760
Tyler <ts576017@ohio.edu>
2022-09-09 15:42:43 -0400

    done

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Tyler Shull
+ *        Date: 9/09/2022
+ * Description: this is part of lab 2 point 15 directions
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
